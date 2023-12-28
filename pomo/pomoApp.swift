2dc287627cc44bb0831c797ae14c47437eb769c6
Slava Zubrin <szubrin79@gmail.com>
2023-12-28 17:45:47 +0200

    Initial Commit

---
+++ pomo/pomoApp.swift
@@ -0,0 +1,17 @@
+//
+//  pomoApp.swift
+//  pomo
+//
+//  Created by Slava Zubrin on 28.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct pomoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
